71ea0addafc7330fbb48ab960d3a7dca046c1668
Ryan Kennedy <ryaninvents@users.noreply.github.com>
2019-02-23 18:16:12 -0500

    fix: Prevent cleanup warning

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ export async function build ({ cwd, out, manifest, reporter, options }) {
   reporter.created(join(out, ZIP_FILENAME), `zip:dependencies`);
 
   try {
-    await del([join(tempWorkingDir, '*')]);
+    await del([tempWorkingDir]);
   } catch (er) {
     // If this fails, it's not a big deal. We'll likely get a non-zero
     // exit code though
